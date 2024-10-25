7a865908fd1b4c8c25874254c004bfb5386d7498
Chirag Gupta <chiragg593@gmail.com>
2024-10-25 15:36:58 +0530

    forms complete

--- packages/ui/src/components/form/index.tsx
+++ packages/ui/src/components/form/index.tsx
@@ -1,4 +1,2 @@
 export { DropzoneInput } from "./DropzoneInput";
 export { DropzonePreview } from "./DropzonePreview";
-// export { default as OrganizationSelect } from "./OrganizationSelect";
-// export { default as SupplierSelect } from "./SupplierSelect";
